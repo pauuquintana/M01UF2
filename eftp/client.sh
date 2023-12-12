2b25ae407017d90d5e1d241e8712bfc6e5f7b8ee
Paula Quintana <paula.quintana@enti.cat>
2023-12-12 11:01:45 +0100

    Cliente y servidor corregido 12/12/23

--- eftp/client.sh
+++ eftp/client.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-IP=`ip address | grep inet | grep -i enp0s3 | cut -d " " -f 6| cut -d "/" -f 1`
 
-SERVER="./client.sh 10.65.0.57"
+if [ $# == 0 ]
+then
+	SERVER="localhost"
+elif [ $# -ge 1]
+then 
+	SERVER="$!"
+fi
+
+IP=`ip address | grep inet | grep enp0s3 | cut -d " " -f 6| cut -d "/" -f 1`
+
 PORT="3333"
 TIMEOUT="1"
 
 echo "Cliente de EFTP"
+
+if [ $# -eq 2 ]
+then
+	echo "(-1) Reset"
+	echo "RESET" | nc $SERVER $PORT
+	sleep 2
+fi
 
 echo "(1) Send"
 
@@ -42,7 +57,29 @@ then
 	exit 2
 fi
 
-echo "(10) Send"
+echo "(9a) SEND NUM_FILES"
+
+NUM_FILES=`ls imgs/ | wc -l`
+ 
+sleep 1
+
+echo "NUM_FILES $NUM_FILES" | nc $SERVER $PORT
+
+echo "(9b) LISTEN OK/KO_NUM_FILES"
+
+DATA=`nc -l -p $PORT -w $TIMEOUT`
+
+if [ "$DATA" != "OK_FILE_NUM" ]
+then
+
+echo "Error 3a: WRONG FILE_NUM"
+exit 3
+fi
+
+for FILE_NAME in `ls imgs/`
+do
+
+echo "(10b) Send"
 
 FILE="fary1.txt"
 MD5=`echo "$FILE" | md5sum | cut -d " " -f 1`
@@ -76,7 +113,7 @@ then
 fi
 
 echo "(18) Send"
-FILE_MD5=`cat $FILE | md5sum | cut -d " " -f 1`
+FILE_MD5=`cat imgs/$FILE_NAME | md5sum | cut -d " " -f 1`
 
 echo "FILE_MD5"
 sleep 1
@@ -92,6 +129,8 @@ then
 	echo "ERROR: FILE MD5"
 	exit 5
 fi
+
+done 
 
 echo "FIN"
 exit 0
--- eftp/server.sh
+++ eftp/server.sh
@@ -15,11 +15,18 @@ echo $DATA
 echo "(3) Test & Send"
 
 PREFIX=`echo $DATA | cut -d " " -f 1`
-VERSION= `echo $DATA | cut -d " " -f 2`
+VERSION=`echo $DATA | cut -d " " -f 2`
 
-if [ "$PREFIX $VERSION" != "EFTP 1.0" ]
+if [ "$PREFIX" != "EFTP" ]
 then
+echo "ERROR 1: BAD HEADER"
+sleep 1
+#echo "KO_HEADER" | nc $CLIENT $PORT 
+exit 1
+fi
 
+if [ "$VERSION" != "1.0" ]
+then
 echo "ERROR 1: BAD HEADER"
 sleep 1
 #echo "KO_HEADER" | nc $CLIENT $PORT 
@@ -28,7 +35,7 @@ fi
 
 CLIENT=`echo $DATA | cut -d " " -f 3`
 
-if [ "$CLIENT" == " "]
+if [ "$CLIENT" == " " ]
 then 
 echo "ERROR: NO IP"
 exit 1
@@ -58,11 +65,40 @@ echo "OK_HANDSHAKE"
 sleep 1
 echo "OK_HANDSHAKE" | nc $CLIENT $PORT
 
-echo "(8) Listen"
+
+echo "(7a) Listen NUM_FILES"
 
 DATA=`nc -l -p $PORT -w $TIMEOUT`
 
-echo "(12) Test & Store & Send"
+echo $DATA
+
+echo "(7b) Send OK/KO_NUM_FILES"
+
+PREFIX=`echo $DATA | cut -d " " -f 1`
+
+if [ "$PREFIX" != "NUM_FILES" ]
+then 
+	echo "ERROR 3: WRONG NUM_FILES PREFIX"
+	sleep 1
+	echo "KO_NUM_FILES" | nc $CLIENT $PORT
+	exit 3
+fi
+
+echo "OK_FILE_NUM" | nc $CLIENT $PORT
+
+NUM_FILES=`echo $DATA | cut -d " " -f 2`
+
+for N in `seq $NUM_FILES`
+do
+
+echo "Archivo número $N"
+
+echo "(8b) Listen"
+
+DATA=`nc -l -p $PORT -w $TIMEOUT`
+
+echo $DATA	
+echo "(12b) Test & Store & Send"
 
 PREFIX=`echo $DATA | cut -d " " -f 1`
 
@@ -81,25 +117,39 @@ sleep 1
 echo "OK_FILE_NAME" | nc $CLIENT $PORT
 echo $FILE_NAME
 
+MD5_CLIENT=`echo $DATA | cut -d " " -f 3`
+MD5_TEST=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
+
+if [ "$MD5" != "$MD5_TEST" ]
+then
+echo "ERROR 5: BAD MD5"
+sleep 1
+echo "KO_MD5" | nc $CLIENT $PORT
+fi
+
+echo "OK_MD5"
+sleep 1
+echo "OK_MD5" | nc $CLIENT $PORT
 
 echo "(13) Listen"
 
-nc -l -p $PORT -w $TIMEOUT > inbox/$FILE_NAME
+DATA=`nc -l -p $PORT -w $TIMEOUT`
 
 echo "(16) Store & Send"
 
-DATA=`cat inbox/$FILE_NAME`
+echo $FILE_NAME
 
-INFO_FILE=`cat $FILE_NAME |wc -w`
+INFO_FILE=`cat $FILE_NAME`
 
-
-if [ "$DATA" == "" ]
+if [ "$INFO_FILE" == "" ]
 then
 	echo "ERROR 4: EMPTY DATA"
 	sleep 1
 	echo "KO_DATA" | nc $CLIENT $PORT
 	exit 4
 fi
+
+echo $DATA > inbox/$FILE_NAME
 
 sleep 1
 echo "OK_DATA" | nc $CLIENT $PORT
@@ -109,12 +159,13 @@ echo "(17) Listen"
 DATA=`nc -l -p $PORT -w $TIMEOUT`
 
 echo "(20) Test & Send"
+echo $DATA
+PREFIX=`echo $DATA | cut -d " " -f 1`
 
-PREFIX=echo $DATA | cut -d " " -f 1
-
+echo $PREFIX
 if [ "$PREFIX" != "FILE_MD5" ]
 then
-    echo "BAD_PREFIX"
+    echo "ERROR: BAD_PREFIX"
     sleep 1
     echo "KO_PREFIX" | nc $CLIENT $PORT
 fi
@@ -132,7 +183,11 @@ then
     echo "KO_FILE_MD5" | nc $CLIENT $PORT
 fi
 
+echo "OK_FILE_MD5"
+sleep 1
 echo "OK_FILE_MD5" | nc $CLIENT $PORT
+
+done
 
 echo "FIN"
 exit 0
